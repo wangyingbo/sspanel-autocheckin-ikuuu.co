27d9fba0862b7e2a43693dddc5a26718a49b6d40
王颖博 <69295750+hiwyb@users.noreply.github.com>
2021-09-30 10:19:35 +0800

    Update ssp-autocheckin.sh

--- ssp-autocheckin.sh
+++ ssp-autocheckin.sh
@@ -63,8 +63,9 @@ else
     
     # 企业微信推送给微信
     echo "企业微信开始推送了"
-    # echo $desp
-    curl -G --data-urlencode "sendkey=wangyingbo" --data-urlencode "msg_type=text" --data-urlencode "to_user=WangYingBo" --data-urlencode "msg=${desp}" 'https://service-d606bcz6-1304203451.usw.apigw.tencentcs.com/release/wecomchan'
+    pushContent=$(echo $desp)
+    echo $pushContent
+    curl -G --data-urlencode "sendkey=wangyingbo" --data-urlencode "msg_type=text" --data-urlencode "to_user=WangYingBo" --data-urlencode "msg=${pushContent}" 'https://service-d606bcz6-1304203451.usw.apigw.tencentcs.com/release/wecomchan'
 fi
 
 push_text="[${date}] ${push_status}"
